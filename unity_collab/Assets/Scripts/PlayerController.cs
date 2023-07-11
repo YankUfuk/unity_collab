bb43882992feb9db314e0c64be9d1f80de0fa172
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2023-07-12 00:13:26 +0300

    added jump and camera follow player

--- unity_collab/Assets/Scripts/PlayerController.cs
+++ unity_collab/Assets/Scripts/PlayerController.cs
@@ -7,14 +7,20 @@ public class PlayerController : MonoBehaviour
     private float horizontal;
     [SerializeField]private float speed = 8.0f;
     [SerializeField]private float jumpForce = 10.0f;
-    private bool isFacingRight = true;
     private float movementX;
     [SerializeField] private Rigidbody2D rb;
+    private bool isGrounded;
+    private string GROUND_TAG = "Ground";
+    private SpriteRenderer sr;
 
-    
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
         PlayerMoveKeyboard();
+        PlayerJump();
     }
 
     void PlayerMoveKeyboard()
@@ -22,5 +28,24 @@ public class PlayerController : MonoBehaviour
         movementX = Input.GetAxisRaw("Horizontal");
 
         transform.position += new Vector3(movementX, 0, 0) * Time.deltaTime * speed;
+    }
+
+
+    void PlayerJump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false;
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if(collision2D.gameObject.CompareTag(GROUND_TAG))
+        {
+            isGrounded = true;
+        }
     }
 }
