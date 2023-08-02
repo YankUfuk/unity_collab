8200a8b7f51b0cd275ab37d28b24214a3705df5d
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2023-08-02 23:47:04 +0300

    NewAssets+Cleaning

--- unity_collab/Assets/Scripts/fps.cs
+++ unity_collab/Assets/Scripts/fps.cs
@@ -8,28 +8,27 @@ public class fps : MonoBehaviour
     public TMP_Text fpsText;
 
     [SerializeField]
-    private float _refreshTime =0.5f;
+    private float _refreshTime = 0.5f;
     private int _framecounter;
     private float _timecounter;
-    private float _fps;
+    private int _fps; // Change the data type to int
 
 
     // Update is called once per frame
     void Update()
     {
-        if(_timecounter<_refreshTime){
-
-            _timecounter+=Time.deltaTime;
+        if (_timecounter < _refreshTime)
+        {
+            _timecounter += Time.deltaTime;
             _framecounter++;
         }
         else
         {
-            {
-                _fps = _framecounter/ _timecounter;
-                _framecounter =0;
-                _timecounter = 0;
-            }
+            _fps = Mathf.RoundToInt(_framecounter / _timecounter); // Round to nearest integer
+            _framecounter = 0;
+            _timecounter = 0;
         }
-        fpsText.text= "FPS: " + _fps.ToString();
+
+        fpsText.text = "FPS: " + _fps.ToString();
     }
 }
