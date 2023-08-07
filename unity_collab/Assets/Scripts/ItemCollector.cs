ba36214db8cb1fd86dad46b9e354099bc8c68c2e
Yankı Ufuk Dülger <yankiudulger@gmail.com>
2023-08-08 00:40:32 +0300

    CollectibleCounterAdded

--- unity_collab/Assets/Scripts/ItemCollector.cs
+++ unity_collab/Assets/Scripts/ItemCollector.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
+    private int collectible = 0;
+    public Text collectibleText;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Collectible"))
         {
             Destroy(collision.gameObject);
+            collectible++;
+            collectibleText.text = "Food: " + collectible;
+
         }
     }
 }
